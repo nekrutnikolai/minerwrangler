43236ec5af1e46a2246f189b98218fdb9bc378e2
miner <nekrutnikolai@gmail.com>
2021-01-05 19:47:27 +0000

    modified the directories

--- Mining Scrips/rvn.sh
+++
@@ -1,3 +0,0 @@
-cd NBMiner
-
-./nbminer -a kawpow -o stratum-ravencoin.flypool.org:3333 -u RJ8QVQtahYHDV5BPgXgccmxKTp3WVLxvoN.rtx2080
--- Mining Scrips/startupmine.sh
+++
@@ -1,5 +0,0 @@
-screen -S rvn -d -m ./rvn.sh
-
-screen -S xmr -d -m  ./xmr.sh
-
-screen -list

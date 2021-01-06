767a8c2e3bac0a544a5c7bfc72b6f1125f97052d
Nikolai Nekrutenko <niknek@pop-os.localdomain>
2021-01-05 23:12:23 -0500

    too tired to do anything more

--- UltimateMinerInstall.sh
+++ UltimateMinerInstall.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/bash
 
 # created by Nikolai Nekrutenko
 
@@ -9,30 +9,46 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-# check with user that the gpus are correctly installed and are the right ones
+#define the confirm function
+confirm() {
+  while true; do
+    read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY
+    case $REPLY in
+      [yY]) echo ; return 0 ;;
+      [nN]) echo ; return 1 ;;
+      *) echo " ${red}invalid input${reset}"
+    esac
+  done
+}
 
+# check with user that the gpus are correctly installed and are the right ones
 vendor=$(lshw -class display | grep 'vendor' | uniq)
-product=$(lshw -class display | grep 'product')
+model=$(lshw -class display | grep 'product')
 
 clear
 
 if [[ $vendor =~ "NVIDIA" ]]; then
-        echo "${green}NVIDIA detected${reset}"
+  echo "${green}NVIDIA GPUs detected${reset}"
+
+elif [[ $vendor =~ "AMD" ]]; then
+  echo "${red}AMD detected but not yet supported :(${reset}"
+
 else
-        echo "${red}No NVIDIA gpu's detected${reset}"
+  echo "${red}No NVIDIA GPUs detected${reset}"
+
 fi
 
-echo "$product"
+echo "$model"
 
-read -n 1 -r -s -p $'Press any key to continue...\n'
+confirm "Does this look good?" || exit 0
 
 # update and upgrade packages to the latest version
 apt update && apt upgrade -y
 
 # install all the necessary libraries
-apt install git unzip build-essential cmake libuv1-dev libssl-dev libhwloc-dev screen xserver-xorg p7zip xorg-dev libgtk-3-dev xdm -y
+apt install git unzip screen xserver-xorg p7zip xorg-dev libgtk-3-dev xdm -y
 
-# allow ssh through firewall, and enable firewall
+# allow ssh through firewall, and enable firewall for security purposes
 ufw allow ssh
 
 ufw enable -y
@@ -89,14 +105,21 @@ mv PhoenixMiner_5.3b_Linux PhoenixMiner
 
 rm PhoenixMiner_5.3b_Linux.tar.gz
 
-#install etherenlargement pill and rename it to ETHPill
-wget https://github.com/Virosa/ETHlargementPill/raw/master/OhGodAnETHlargementPill-r2
+#ask user if the pill shall be installed? etherenlargement pill and rename it to ETHPill
+pill() {
+  wget https://github.com/Virosa/ETHlargementPill/raw/master/OhGodAnETHlargementPill-r2
 
-chmod +x OhGodAnETHlargementPill-r2
+  chmod +x OhGodAnETHlargementPill-r2
 
-rm OhGodAnETHlargementPill-r2 ETHPill
+  mv OhGodAnETHlargementPill-r2 ETHPill
+}
+
+printf '\U1F48A'&& confirm "The pill?" && pill
 
 # install of that xmrig shiete
+
+apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+
 git clone https://github.com/xmrig/xmrig.git
 
 mkdir xmrig/build && cd xmrig/build
